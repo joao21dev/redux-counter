f1cabfa5e300d00a381770ca5db3e2f5121c6975
joao21dev <joao21dev@gmail.com>
2022-03-29 11:55:08 -0300

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { increment, decrement } from "./Actions";
+
+const Counter = ({ count, increment, decrement }) => {
+  return (
+    <p>
+      Counter: {count}
+      <button onClick={increment}> +</button>
+      <button onClick={decrement}>-</button>
+    </p>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch(increment),
+    decrement: () => dispatch(decrement),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
